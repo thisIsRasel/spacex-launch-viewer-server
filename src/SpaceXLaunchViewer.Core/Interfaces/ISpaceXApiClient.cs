8da645af988f460e61f918143cf7a90670f08209
Rasel <rasel.ahammed@wcgclinical.com>
2023-05-20 11:40:43 +0600

    Client service updated to get launch detail

--- src/SpaceXLaunchViewer.Core/Interfaces/ISpaceXApiClient.cs
+++ src/SpaceXLaunchViewer.Core/Interfaces/ISpaceXApiClient.cs
@@ -5,6 +5,8 @@ using SpaceXLaunchViewer.Core.ResponseModels;
 namespace SpaceXLaunchViewer.Core.Interfaces;
 public interface ISpaceXApiClient
 {
+    Task<Result<LaunchDetail?>> GetLaunchByFlightNumberAsync(LaunchDetailQuery query);
+
     Task<Result<IEnumerable<Launch>?>> GetPastLaunchesAsync(LaunchQuery query);
 
     Task<Result<IEnumerable<Launch>?>> GetUpcomingLaunchesAsync(LaunchQuery query);
--- src/SpaceXLaunchViewer.Infrastructure/Services/SpaceXApiClient.cs
+++ src/SpaceXLaunchViewer.Infrastructure/Services/SpaceXApiClient.cs
@@ -21,13 +21,40 @@ public sealed class SpaceXApiClient : ISpaceXApiClient
 
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly IValidator<LaunchQuery> _launchQueryValidator;
+    private readonly IValidator<LaunchDetailQuery> _launchDetailQueryValidator;
 
     public SpaceXApiClient(
         IHttpClientFactory httpClientFactory,
-        IValidator<LaunchQuery> launchQueryValidator)
+        IValidator<LaunchQuery> launchQueryValidator,
+        IValidator<LaunchDetailQuery> launchDetailQueryValidator)
     {
         _httpClientFactory = httpClientFactory;
         _launchQueryValidator = launchQueryValidator;
+        _launchDetailQueryValidator = launchDetailQueryValidator;
+    }
+
+    public async Task<Result<LaunchDetail?>> GetLaunchByFlightNumberAsync(
+        LaunchDetailQuery query)
+    {
+        var validationResult = await _launchDetailQueryValidator.ValidateAsync(query);
+        if (validationResult.IsValid is false)
+        {
+            return new Result<LaunchDetail?>(
+                new ValidationException(validationResult.Errors));
+        }
+
+        using var httpClient = _httpClientFactory.CreateClient(SpaceXOptions.SpaceX);
+
+        var response = await RetryPolicy
+            .ExecuteAsync(() => httpClient.GetAsync($"launches/{query.FlightNumber}"));
+
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<LaunchDetail>(content);
+        }
+
+        return null;
     }
 
     public async Task<Result<IEnumerable<Launch>?>> GetPastLaunchesAsync(
